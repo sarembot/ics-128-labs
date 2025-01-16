b7681cb402dc8e51ba6985c12cc383ec8925b92e
sarembot <sarembot@github.com>
2025-01-16 07:50:38 -0800

    Added message to DOM.

---
+++ includes/js/lab02.js
@@ -0,0 +1,12 @@
+// ICS128 Lab02 - Mitchell Saremba
+
+console.log("working");
+const messageDiv = document.getElementById("message");
+const message = document.createElement("h2");
+message.innerText = "Please provide 3 prices for rooms.";
+
+messageDiv.appendChild(message);
+
+// const numOne = prompt("Enter a number");
+// const numTwo = prompt();
+// const numThree = prompt();
