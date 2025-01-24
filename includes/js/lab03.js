9d44c69770367c4ca1db604064cb99b89bb060c3
Mitchell Saremba <mitchellsaremba@proton.me>
2025-01-23 17:58:37 -0800

    Initial counter functionality.

--- includes/js/lab03.js
+++ includes/js/lab03.js
@@ -1,26 +1,50 @@
 // ICS128 LAB03 - Mitchell Saremba
 const countersBtn = document.getElementById("countersBtn");
+const dateBtn = document.getElementById("dateBtn");
+
 const spacesDisplay = document.getElementById("spacesDisplay");
 const lettersDisplay = document.getElementById("lettersDisplay");
 
+const spacesInput = document.getElementById("spaces");
+const lettersInput = document.getElementById("letters");
+
+// TODO: Use spacesInput and add "input" event
 countersBtn.addEventListener("click", () => {
   const spacesVal = document.getElementById("spaces").value;
   const lettersVal = document.getElementById("letters").value;
+  const spacesSpan = document.getElementById("spacesSpan");
 
-  const counts = counter(spacesVal, "L");
-  console.log(counts);
+  // counts[0] --> spacesCount, counts[1] --> lettersCount
+  const [spacesCount, lettersCount] = counter(spacesVal, "L");
+
+  // Clear DOM
+  // spacesDisplay.innerHTML = "";
+  // lettersDisplay.innerHTML = "";
+
+  spacesSpan.classList.add("text-danger");
+  spacesSpan.classList.add("fw-bold");
+  spacesSpan.textContent = spacesCount;
+
+  console.log(typeof document.getElementById("spacesP").textContent);
+
+  lettersSpan.classList.add("text-success");
+  lettersSpan.classList.add("fw-bold");
+  lettersSpan.textContent = lettersCount;
+
+  lettersVal = `${document.getElementById("spacesP").textContent}`;
 });
 
+// lettersInput.addEventListener("input", () => {
+//   console.log("working");
+// });
+
 function counter(str, char) {
-  const counts = [];
   let spaceCount = 0;
   let charCount = 0;
 
   for (c of str) if (c === " ") spaceCount += 1;
-  for (c of str) if (c === char) charCount += 1;
+  for (c of str)
+    if (c.toUpperCase() === char || c.toLowerCase() == char) charCount += 1;
 
-  counts.push(spaceCount);
-  counts.push(charCount);
-
-  return counts;
+  return [spaceCount, charCount];
 }
