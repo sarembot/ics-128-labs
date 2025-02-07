8cf9c24f74e8348fac7defdaa0a79c60dfd460a6
Mitchell Saremba <mitchellsaremba@proton.me>
2025-02-06 16:42:01 -0800

    Started messing around with updating display of input dynamically

--- includes/js/lab05.js
+++ includes/js/lab05.js
@@ -2,16 +2,135 @@
 "use strict";
 const formBtn = document.getElementById("formBtn");
 
+const fName = fNameInput.value;
+const lName = lNameInput.value;
+
+class Input {
+  constructor(name, input, icon, iconSpan, regex) {
+    this.name = name;
+    this.input = input;
+    this.icon = icon;
+    this.iconDiv = iconSpan;
+    this.value = this.input.value; // needs to update dynamically
+    this.regex = regex;
+  }
+
+  updateValue() {
+    this.value = this.input.value;
+  }
+}
+
+let inputObjs = [
+  new Input(
+    "fName",
+    document.getElementById("fNameInput"),
+    document.getElementById("fNameIcon"),
+    document.getElementById("fNameIconDiv"),
+    /^[a-za-z]+$/
+  ),
+
+  //   {
+  //     name: "fName",
+  //     input: document.getElementById("fNameInput"),
+  //     icon: document.getElementById("fNameIcon"),
+  //     value: `${this.input.value}`,
+  //     regex: /^[a-za-z]+$/,
+  //   },
+  //   {
+  //     name: "lName",
+  //     input: document.getElementById("lNameInput"),
+  //     icon: document.getElementById("lNameIcon"),
+  //     regex: /^[a-za-z]+$/,
+  //   },
+  //   {
+  //     name: "email",
+  //     input: document.getElementById("emailInput"),
+  //     icon: document.getElementById("emailIcon"),
+  //   },
+  //   {
+  //     name: "age",
+  //     input: document.getElementById("ageInput"),
+  //     icon: document.getElementById("ageIcon"),
+  //   },
+  //   {
+  //     name: "postalCode",
+  //     input: document.getElementById("postalCodeInput"),
+  //     icon: document.getElementById("postalCodeIcon"),
+  //   },
+  //   {
+  //     name: "phoneNumber",
+  //     input: document.getElementById("phoneNumberInput"),
+  //     icon: document.getElementById("phoneNumberIcon"),
+  //   },
+];
+
+let goodInputClassList = [
+  "border",
+  "border-4",
+  "border-rounded",
+  "focus-ring",
+  "focus-ring-success",
+  "border-success",
+  "border-opacity-50",
+];
+
+let badInputClassList = [
+  "border",
+  "border-4",
+  "border-rounded",
+  "focus-ring",
+  "focus-ring-danger",
+  "border-danger",
+  "border-opacity-50",
+];
+
+inputObjs.forEach((obj) => {
+  obj.input.addEventListener("input", () => {
+    obj.updateValue(); // So input value is updated dynamically
+
+    // If input passes regex test
+    if (obj.regex.test(obj.value)) {
+      // Update input color
+      obj.input.classList.remove(...badInputClassList);
+      obj.iconDiv.classList.remove(...badInputClassList);
+
+      obj.input.classList.add(...goodInputClassList);
+      obj.iconDiv.classList.add(...goodInputClassList);
+
+      obj.icon.classList.remove("bi-person-fill-exclamation");
+      obj.icon.classList.remove("bi-person-dash-fill");
+      obj.icon.classList.add("bi-person-check-fill");
+    } else {
+      obj.input.classList.remove(...goodInputClassList);
+      obj.iconDiv.classList.remove(...goodInputClassList);
+
+      obj.input.classList.add(...badInputClassList);
+      obj.iconDiv.classList.add(...badInputClassList);
+
+      obj.icon.classList.remove("bi-person-check-fill");
+      obj.icon.classList.add("bi-person-fill-exclamation");
+    }
+  });
+});
+
+// inputs.forEach((input) => {
+//   input.addEventListener("input", () => {
+//     console.log("fire");
+//     if (nameregex.test(fName)) {
+//       input.className = "form-control border border-3 border-success";
+//       icon;
+//     } else {
+//       input.className = "form-control";
+//     }
+//   });
+// });
+
 // Make the emoji's to the left of inputs an 'x', and then when the user enters a valid input, make it a green check
 
 console.log("working");
 formBtn.addEventListener("click", () => {
-  const fName = document.getElementById("firstNameInput").value;
-  const lName = document.getElementById("lastNameInput").value;
-  const nameRegex = /^[A-Za-z]+$/;
-
-  if (nameRegex.test(fName)) console.log("First Name Valid");
-  if (nameRegex.test(lName)) console.log("Last Name Valid");
+  if (nameRegex.test(fName)) console.log("first name valid");
+  if (nameRegex.test(lName)) console.log("last name valid");
 
   const email = document.getElementById("emailInput").value;
   // TODO: make email re better - don't allow more than one special char in a row, etc;
