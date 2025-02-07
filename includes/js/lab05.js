903e3ae3c4c704f6cf1005062c49b1ee045d64c4
Mitchell Saremba <mitchellsaremba@proton.me>
2025-02-07 13:39:57 -0800

    Added full regex validation for inputs

--- includes/js/lab05.js
+++ includes/js/lab05.js
@@ -13,12 +13,21 @@ class Input {
     this.iconDiv = iconSpan;
     this.value = this.input.value; // needs to update dynamically
     this.regex = regex;
+    this.errorMsg = errorMsg;
   }
+
+  error = new Error(this.errorMsg);
 
   updateValue() {
     this.value = this.input.value;
   }
 }
+
+const inputIcons = {
+  neutral: "fs-5 bi-dash-circle",
+  good: "fs-5 bi-check-circle",
+  bad: "fs-5 bi-x-circle",
+};
 
 let inputObjs = [
   new Input(
@@ -26,42 +35,45 @@ let inputObjs = [
     document.getElementById("fNameInput"),
     document.getElementById("fNameIcon"),
     document.getElementById("fNameIconDiv"),
-    /^[a-za-z]+$/
+    /^[a-zA-Z]+$/
   ),
-
-  //   {
-  //     name: "fName",
-  //     input: document.getElementById("fNameInput"),
-  //     icon: document.getElementById("fNameIcon"),
-  //     value: `${this.input.value}`,
-  //     regex: /^[a-za-z]+$/,
-  //   },
-  //   {
-  //     name: "lName",
-  //     input: document.getElementById("lNameInput"),
-  //     icon: document.getElementById("lNameIcon"),
-  //     regex: /^[a-za-z]+$/,
-  //   },
-  //   {
-  //     name: "email",
-  //     input: document.getElementById("emailInput"),
-  //     icon: document.getElementById("emailIcon"),
-  //   },
-  //   {
-  //     name: "age",
-  //     input: document.getElementById("ageInput"),
-  //     icon: document.getElementById("ageIcon"),
-  //   },
-  //   {
-  //     name: "postalCode",
-  //     input: document.getElementById("postalCodeInput"),
-  //     icon: document.getElementById("postalCodeIcon"),
-  //   },
-  //   {
-  //     name: "phoneNumber",
-  //     input: document.getElementById("phoneNumberInput"),
-  //     icon: document.getElementById("phoneNumberIcon"),
-  //   },
+  new Input(
+    "lName",
+    document.getElementById("lNameInput"),
+    document.getElementById("lNameIcon"),
+    document.getElementById("lNameIconDiv"),
+    /^[a-zA-Z]+$/
+  ),
+  new Input(
+    "email",
+    document.getElementById("emailInput"),
+    document.getElementById("emailIcon"),
+    document.getElementById("emailIconDiv"),
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  ),
+  new Input(
+    "age",
+    document.getElementById("ageInput"),
+    document.getElementById("ageIcon"),
+    document.getElementById("ageIconDiv"),
+    /^[0-9]{1,2}$/
+  ),
+  new Input(
+    "postalCode",
+    document.getElementById("postalCodeInput"),
+    document.getElementById("postalCodeIcon"),
+    document.getElementById("postalCodeIconDiv"),
+    // No D,F,I,O,Q,U
+    // Can't start with W or Z
+    /^[a-ceg-hj-npr-vyA-CEG-HJ-NPR-VY][0-9][a-ceg-hj-npr-zA-CEG-HJ-NPR-Z][\s]?[0-9][a-ceg-hj-npr-zA-CEG-HJ-NPR-Z][0-9]$/
+  ),
+  new Input(
+    "phoneNumber",
+    document.getElementById("phoneNumberInput"),
+    document.getElementById("phoneNumberIcon"),
+    document.getElementById("phoneNumberIconDiv"),
+    /^\(?[0-9]{3}\)?[-\s]?[0-9]{3}[-\s]?[0-9]{4}$/
+  ),
 ];
 
 let goodInputClassList = [
@@ -84,72 +96,42 @@ let badInputClassList = [
   "border-opacity-50",
 ];
 
+// Validates input dynamically
 inputObjs.forEach((obj) => {
   obj.input.addEventListener("input", () => {
-    obj.updateValue(); // So input value is updated dynamically
+    obj.updateValue(); // Dynamically update value
 
-    // If input passes regex test
-    if (obj.regex.test(obj.value)) {
-      // Update input color
+    // Empty input
+    if (obj.value === "") {
+      obj.input.classList.remove(...goodInputClassList);
       obj.input.classList.remove(...badInputClassList);
-      obj.iconDiv.classList.remove(...badInputClassList);
 
+      obj.icon.className = inputIcons.neutral; // className clears prev classes
+
+      // Good input
+    } else if (obj.regex.test(obj.value)) {
+      obj.input.classList.remove(...badInputClassList);
       obj.input.classList.add(...goodInputClassList);
-      obj.iconDiv.classList.add(...goodInputClassList);
 
-      obj.icon.classList.remove("bi-person-fill-exclamation");
-      obj.icon.classList.remove("bi-person-dash-fill");
-      obj.icon.classList.add("bi-person-check-fill");
+      obj.icon.className = inputIcons.good;
+
+      // Bad input
     } else {
       obj.input.classList.remove(...goodInputClassList);
-      obj.iconDiv.classList.remove(...goodInputClassList);
-
       obj.input.classList.add(...badInputClassList);
-      obj.iconDiv.classList.add(...badInputClassList);
 
-      obj.icon.classList.remove("bi-person-check-fill");
-      obj.icon.classList.add("bi-person-fill-exclamation");
+      obj.icon.className = inputIcons.bad;
     }
   });
 });
 
-// inputs.forEach((input) => {
-//   input.addEventListener("input", () => {
-//     console.log("fire");
-//     if (nameregex.test(fName)) {
-//       input.className = "form-control border border-3 border-success";
-//       icon;
-//     } else {
-//       input.className = "form-control";
-//     }
-//   });
-// });
-
-// Make the emoji's to the left of inputs an 'x', and then when the user enters a valid input, make it a green check
-
-console.log("working");
+// Validates form after submission
 formBtn.addEventListener("click", () => {
-  if (nameRegex.test(fName)) console.log("first name valid");
-  if (nameRegex.test(lName)) console.log("last name valid");
-
-  const email = document.getElementById("emailInput").value;
-  // TODO: make email re better - don't allow more than one special char in a row, etc;
-  //   const emailRegex = /^[\w\.]+@[\w\.]+\.[a-zA-Z]$/;
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-  if (emailRegex.test(email)) console.log("Email Valid");
-
-  const age = document.getElementById("ageInput").value;
-  const ageRegex = /^[0-9]{1,2}$/;
-
-  if (ageRegex.test(age)) console.log("Age Valid");
-  else console.log("Age not valid");
-
-  const postalCode = document.getElementById("postalCodeInput").value;
-
-  const phoneNumber = document.getElementById("phoneNumInput").value;
-  const phoneRegex = /^\(?[0-9]{3}\)?[-\s]?[0-9]{3}[-\s]?[0-9]{4}$/;
-
-  if (phoneRegex.test(phoneNumber)) console.log("Phone Valid");
-  else console.log("Phone not valid");
+  for (obj in inputObjs) {
+    if (obj.regex.test(obj.value)) {
+      continue;
+    } else {
+      throw this.error;
+    }
+  }
 });
